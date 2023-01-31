5467c4d2712b03253ef8e393c5c069dc678b9f61
Josh <millsapcyber@gmail.com>
2023-01-31 13:52:39 -0600

    refactor & documentation

--- main.py
+++ main.py
@@ -1,42 +1,56 @@
-from email.mime import base
-from multiprocessing import reduction
-from xml.dom.minidom import Element
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
-from subprocess import Popen
 from copy import deepcopy
-
-import sys
 import re
 import time
 import json
 import requests
 
-def extractHARFilters(link):
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+
+
+def extract_har_filters(link):
+    """extract_har_filters parses filters out of a har link
+
+    Args:
+        link (string): Generate a search on har.com and copy the link from the browser
+
+    Returns:
+        json: a json representation of all the get parameters from the har search
+    """
     retval = {}
     if not link.startswith("https://www.har.com/search/dosearch?"):
         print("Incorrect base link provided")
-        exit(1)
-    link = link.replace("https://www.har.com/search/dosearch?", "")
+        print("You may be searching by city, try searching by map bounds instead")
+    link = re.sub("https:\/\/www\.har\.com\/.*\?", "", link)
     args = link.split("&")
     for arg in args:
         tmp = arg.split("=")
         retval[tmp[0]] = tmp[1]
     return retval
 
-def getListings(args):
+
+def get_listings(args):
+    """get_listings Scrapes a har search page for the links to each listing.
+    It also traverses multi-page searches.
+
+    Args:
+        args (json): a json representation of a har.com filter
+
+    Returns:
+        string[]: all the links of each listing withing the specified search.
+    """
     links = []
     page_number = 1
 
-
     options = Options()
-    options.add_argument('--headless')
+    options.add_argument("--headless")
     browser = webdriver.Firefox(options=options)
 
-
-    while(True):
-        target_url = "https://www.har.com/search/dosearch?view=list&page={}".format(str(page_number))
+    while True:
+        target_url = "https://www.har.com/search/dosearch?view=list&page={}".format(
+            str(page_number)
+        )
 
         # build url with desired querys
         args_list = list(args.keys())
@@ -45,12 +59,14 @@ def getListings(args):
 
         # call HAR
         browser.get(target_url)
-        
+
         # check if there are any listings available
-        if(browser.find_element(By.XPATH, '//*[@id="noresults_message"]').is_displayed()):
+        if browser.find_element(
+            By.XPATH, '//*[@id="noresults_message"]'
+        ).is_displayed():
             break
         else:
-            print("Harvesting listings from page: {}".format(page_number), end='\r')
+            print("Harvesting listings from page: {}".format(page_number), end="\r")
 
             elements = browser.find_elements(By.CLASS_NAME, "listing-card-item")
             for element in elements:
@@ -60,13 +76,25 @@ def getListings(args):
         page_number = page_number + 1
     browser.close()
     if len(links) == 0:
-        print("There are no listings within your criteria. Please adjust filters and try again.")
+        print(
+            "There are no listings within your criteria. Please adjust filters and try again."
+        )
         exit(1)
+    print()
     return links
 
-def evaluateListing(link):
+
+def evaluate_listing(link):
+    """evaluate_listing Scrapes data from an individual listing on har.com into json
+
+    Args:
+        link (string): the link of an individual listing on har.com
+
+    Returns:
+        json: all the json data for the listing
+    """
     options = Options()
-    options.add_argument('--headless')
+    options.add_argument("--headless")
     browser = webdriver.Firefox(options=options)
 
     browser.get(link)
@@ -74,97 +102,179 @@ def evaluateListing(link):
     # get rows
     rows = browser.find_elements(By.CLASS_NAME, "row")
 
-    evaluatedListing = {}
-    evaluatedListing['link'] = link
+    evaluated_listing = {}
+    evaluated_listing["link"] = link
     for row in rows:
-        if "MLS#" in row.text and "List Price" in row.text and "Listing Status" in row.text and "Address" in row.text and "City" in row.text and "State" in row.text and "Zip Code" in row.text and "County" in row.text and "Subdivision" in row.text and "Legal Description" in row.text and "Property Type" in row.text and "Bedroom" in row.text and "Bath" in row.text and "Garages" in row.text and "Stories" in row.text and "Building Size" in row.text and "Architecture Style" in row.text and "Year Built" in row.text and "Lot Size" in row.text and "Key Map©" in row.text and "Market Area" in row.text: 
-            data = row.text.split('\n')
+        if (
+            "MLS#" in row.text
+            and "List Price" in row.text
+            and "Listing Status" in row.text
+            and "Address" in row.text
+            and "City" in row.text
+            and "State" in row.text
+            and "Zip Code" in row.text
+            and "County" in row.text
+            and "Subdivision" in row.text
+            and "Legal Description" in row.text
+            and "Property Type" in row.text
+            and "Bedroom" in row.text
+            and "Bath" in row.text
+            and "Garages" in row.text
+            and "Stories" in row.text
+            and "Building Size" in row.text
+            and "Architecture Style" in row.text
+            and "Year Built" in row.text
+            and "Lot Size" in row.text
+            and "Key Map©" in row.text
+            and "Market Area" in row.text
+        ):
+            data = row.text.split("\n")
             i = 0
             for i in range(0, len(data) - 1):
-                
+
                 if i % 2 == 0:
-                    evaluatedListing[data[i]] = ''
-                else: 
-                    evaluatedListing[data[i - 1]] = data[i]
+                    evaluated_listing[data[i]] = ""
+                else:
+                    evaluated_listing[data[i - 1]] = data[i]
         if re.search("[0-9]+x[0-9]+", row.text):
-            data = row.text.replace(', ', '\n').replace(' ', '\n').split('\n')
+            data = row.text.replace(", ", "\n").replace(" ", "\n").split("\n")
             i = 0
             for i in range(0, len(data) - 1):
-                
+
                 if i + 1 % 3 == 2:
-                    evaluatedListing[data[i - 1] + " " + data[i]] = ''
-                if (i + 1) % 3 == 0: 
-                    evaluatedListing[data[i - 2] + " " + data[i - 1]] = data[i]
-        if "Roof" in row.text and "Foundation" in row.text and "Private Pool" in row.text and "Exterior Type" in row.text and "Water Sewer" in row.text and "Area Pool" in row.text:
-            data = row.text.split('\n')
+                    evaluated_listing[data[i - 1] + " " + data[i]] = ""
+                if (i + 1) % 3 == 0:
+                    evaluated_listing[data[i - 2] + " " + data[i - 1]] = data[i]
+        if (
+            "Roof" in row.text
+            and "Foundation" in row.text
+            and "Private Pool" in row.text
+            and "Exterior Type" in row.text
+            and "Water Sewer" in row.text
+            and "Area Pool" in row.text
+        ):
+            data = row.text.split("\n")
             i = 0
             for i in range(0, len(data) - 1):
-                
+
                 if i % 2 == 0:
-                    evaluatedListing[data[i]] = ''
-                else: 
-                    evaluatedListing[data[i - 1]] = data[i]
-            # print(evaluatedListing)
-        if "Fireplace" in row.text and "Floors" in row.text and "Bathroom Description" in row.text and "Bedroom Description" in row.text and "Room Description" in row.text and "Cooling" in row.text and "Heating" in row.text and "Dishwasher" in row.text and "Disposal" in row.text and "Oven" in row.text and "Appliances" in row.text: 
-            data = row.text.split('\n')
+                    evaluated_listing[data[i]] = ""
+                else:
+                    evaluated_listing[data[i - 1]] = data[i]
+            # print(evaluated_listing)
+        if (
+            "Fireplace" in row.text
+            and "Floors" in row.text
+            and "Bathroom Description" in row.text
+            and "Bedroom Description" in row.text
+            and "Room Description" in row.text
+            and "Cooling" in row.text
+            and "Heating" in row.text
+            and "Dishwasher" in row.text
+            and "Disposal" in row.text
+            and "Oven" in row.text
+            and "Appliances" in row.text
+        ):
+            data = row.text.split("\n")
             i = 0
             for i in range(0, len(data) - 1):
-                
+
                 if i % 2 == 0:
-                    evaluatedListing[data[i]] = ''
-                else: 
-                    evaluatedListing[data[i - 1]] = data[i]
-
-            
-
+                    evaluated_listing[data[i]] = ""
+                else:
+                    evaluated_listing[data[i - 1]] = data[i]
 
     browser.close()
-    return evaluatedListing
+    return evaluated_listing
 
-def evaluateListings(links):
-    evaluatedListings = []
+
+def evaluate_listings(links):
+    """evaluate_listings Calls evaluate_listing on an array of links
+
+    Args:
+        links (string[]): An array of strings containing har.com listing links
+
+    Returns:
+        json array: a json array of all listings that were passed in the links array
+    """
+    evaluated_listings = []
     i = 1
     for link in links:
-        evaluatedListings.append(deepcopy(evaluateListing(link)))
-        print("evaluated listing {} of {}".format(i, len(links)), end='\r')
+        evaluated_listings.append(deepcopy(evaluate_listing(link)))
+        print("evaluated listing {} of {}".format(i, len(links)), end="\r")
 
         i = i + 1
-    return evaluatedListings
-    
-def printListings(evaluated_listings):
+    print()
+    return evaluated_listings
+
+
+def print_listings(evaluated_listings):
+    """print_listings a print function for arrays of json objects
+
+    Args:
+        evaluated_listings (json[]): jsonp[] returned by evaluate_listings
+    """
+    if len(evaluate_listings) == 0:
+        print("No listings found.")
     for listing in evaluated_listings:
         print(json.dumps(listing, indent=2))
 
-def enrichListingsWithFiberData(evaluated_listings):
+
+def enrich_listings_with_fiber_data(evaluated_listings):
+    """enrich_listings_with_fiber_data add a bool to each listings json indicating wether or not the listing has fiber internet available
+
+    Args:
+        evaluated_listings (json[]): json[] returned by evaluate_listings
+
+    Returns:
+        _type_: _description_
+    """
     retval = []
-    payload={}
+    payload = {}
     headers = {}
 
     for listing in evaluated_listings:
-        url = "https://www.att.com/services/shop/model/ecom/shop/view/unified/qualification/service/CheckAvailabilityRESTService/invokeCheckAvailability?userInputZip={}&userInputAddressLine1={}&mode=fullAddress".format(listing['Zip Code'], listing['Address'])
-        response_data = json.loads(requests.request("GET", url, headers=headers, data=payload).text)
-        listing['Fiber'] = response_data['profile']['isGIGAFiberAvailable']
+        url = "https://www.att.com/services/shop/model/ecom/shop/view/unified/qualification/service/CheckAvailabilityRESTService/invokeCheckAvailability?userInputZip={}&userInputAddressLine1={}&mode=fullAddress".format(
+            listing["Zip Code"], listing["Address"]
+        )
+        response_data = json.loads(
+            requests.request("GET", url, headers=headers, data=payload).text
+        )
+        listing["Fiber"] = response_data["profile"]["isGIGAFiberAvailable"]
         retval.append(listing)
         # artifical wait to avoid api rate limiting
         time.sleep(1)
-        
+
     return retval
 
-def filterListingsByFiber(evaluated_listings):
+
+def filter_listings_by_fiber(evaluated_listings):
+    """filter_listings_by_fiber only returns listings that have fiber service, disgards the rest.
+
+    Args:
+        evaluated_listings (json[]): an array of json objects generated by evaluate_listings
+
+    Returns:
+        json[]: the same as the passed argument but without any listings that don't have fiber
+    """
     retval = []
     for listing in evaluated_listings:
-        if listing['Fiber'] == True:
+        if listing["Fiber"]:
             retval.append(listing)
     return retval
 
-if __name__ == "__main__":
-    # set your filters up in har, then pass that link into extractHARFilters like in the following example, which searches every listing in texas for a 3 bed 2 bath <= $250,000
-    args = extractHARFilters("https://www.har.com/search/dosearch?for_sale=1&listing_price_max=250000&bedroom_min=3&full_bath_min=2&property_class_id=1")    
-    # apply filters and gather listings
-    listings = getListings(args)
-    # scrape data from gathered listings
-    evaluated_listings = evaluateListings(listings)
-    # enrich data with fiber availability
-    evaluated_listings = enrichListingsWithFiberData(evaluated_listings)
 
-    printListings(evaluated_listings)
+if __name__ == "__main__":
+    # set your filters up in har, then pass that link into extract_har_filters 
+    # which searches every listing in texas for a 3 bed 2 bath <= $250,000
+    arguments = extract_har_filters(
+        "https://www.har.com/katy/realestate/for_sale?listing_price_max=250000&bedroom_min=3&full_bath_min=2&all_status=A&sort=listdate%20desc&view=list"
+    )
+    # apply filters and gather listings
+    listings = get_listings(arguments)
+    # scrape data from gathered listings
+    processed_listings = evaluate_listings(listings)
+    # enrich data with fiber availability
+    processed_listings = enrich_listings_with_fiber_data(processed_listings)
+
+    print_listings(processed_listings)
